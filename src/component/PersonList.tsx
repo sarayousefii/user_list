0ba5e7d48e1bba672166293ff19a895232da4ba2
sara yousefi <sara.youseffi87@gmail.com>
2025-10-31 22:18:36 +0330

    edit style

--- src/component/PersonList.tsx
+++ src/component/PersonList.tsx
@@ -23,13 +23,13 @@ const PersonList: FC<Iprops> = ({ persons , setPersons}) => {
             <div className="card mb-3" >
                 <div className="row g-0">
                     
-                    <div className="col-md-10">
+                    <div className="col-10 ">
                         <div className="card-body">
                             <h5 className="card-title">{person.firstName +" "+person.lastName}</h5>
                             <p className="card-text"><small className="text-body-secondary">{format(person.date.toISOString(), "yyyy-MM-dd h:m")}</small></p>
                         </div>
                     </div>
-                    <div className="col-md-2 d-flex flex-column justify-content-center">
+                    <div className="col-2 d-flex flex-column justify-content-center">
                         <MdDelete className="m-2"  style={{cursor:"pointer"}} onClick={()=>handelDelete(person.id)}/>
                         <EditUser person={person} persons={persons} setPersons={setPersons}/>
                     </div>
