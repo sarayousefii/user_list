08e5a945cccabb6fda48cdb7d1acba8433fe75c6
Sara <sara.youseffi87@gmail.com>
2025-11-03 03:50:58 +0330

    add form validation

--- src/component/AddUser.tsx
+++ src/component/AddUser.tsx
@@ -1,45 +1,55 @@
 import { useState, FormEvent, Dispatch, SetStateAction, FC } from "react"
 import { Iperson } from "../App";
 import Modal from 'react-bootstrap/Modal';
+import { Formik, Field, Form, FormikHelpers,ErrorMessage } from 'formik';
+import * as Yup from "yup";
 
 interface Iprops {
     persons: Iperson[];
     setPersons: Dispatch<SetStateAction<Iperson[]>>;
 }
 
+interface MyFormValues {
+  firstname: string;
+  lastname: string;
+}
+
 const AddUser: FC<Iprops> = ({ persons, setPersons }) => {
-    const [firstName, setfirstName] = useState<string>("");
-    const [lastName, setLastname] = useState<string>("");
-    
 
     const [isShowModal, setIsShowModal] = useState<boolean>(false);
     const handleClose = () => {
-        setfirstName("");
-        setLastname("");
         setIsShowModal(false);
     };
     const handleShow = () => setIsShowModal(true);
+    
+    const validationSchema = Yup.object({
+        firstname: Yup.string().required(" first name is required "),
+        lastname: Yup.string().required(" last name is required ")
+    });
 
-    const handledSubmitForm = (event: FormEvent<HTMLFormElement>): void => {
+    const handledSubmitForm = async (
+        values: MyFormValues,
+        { setSubmitting, resetForm }: FormikHelpers<MyFormValues>
+        ) => {
+            if(isShowModal){
+                setPersons([
+                    ...persons,
+                    {
+                        id:Math.floor(Math.random()*1000000),
+                        firstName:values.firstname,
+                        lastName:values.lastname,
+                        date:new Date(),
+                    }
+                ])
+                setIsShowModal(false)
+            }
 
-        if(isShowModal && firstName && lastName){
-            event.preventDefault();
-            setPersons([
-                ...persons,
-                {
-                    id:Math.floor(Math.random()*1000000),
-                    firstName,
-                    lastName,
-                    date:new Date(),
-                }
-            ])
-            event.preventDefault();
-            setfirstName("");
-            setLastname("");
-            setIsShowModal(false)
-        }
-    }
+        setSubmitting(false);
+        resetForm();
+    };
 
+  
+    const initialValues: MyFormValues = { firstname: '', lastname: '' };
     return (
         <>
             <button className="btn btn-secondary mb-2" onClick={handleShow}>create</button>
@@ -50,40 +60,41 @@ const AddUser: FC<Iprops> = ({ persons, setPersons }) => {
                 keyboard={false}
             >
                 <Modal.Body>
-                    <form autoComplete="off" className="container border rounded p-4" onSubmit={(e) => { handledSubmitForm(e) }}>
-                        <div className="row justify-content-center mb-3">Create user form</div>
-                        <div className="row">
-                            <div className="col">
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="Enter firstname"
-                                    name="firstname"
-                                    value={firstName}
-                                    onChange={(e) => setfirstName(e.target.value)}
-                                    required={true}
-                                />
-                            </div>
-                            <div className="col">
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="Enter last name"
-                                    name="lastname"
-                                    onChange={(e) => setLastname(e.target.value)}
-                                    value={lastName}
-                                    required={true}
-                                />
-                            </div>
-                        </div>
+                    <Formik
+                        initialValues={initialValues}
+                        validationSchema={validationSchema}
+                        onSubmit={handledSubmitForm}
+                    >       
+                        {({ setFieldValue }) => (
+                            <Form autoComplete="off" className="container border rounded p-4" >
+                                <div className="row justify-content-center mb-3">Create user form</div>
+                                <div className="row">
+                                    <div className="col">
+                                        <label>
+                                            <Field type="text" name="firstname"  placeholder="First Name" />
+                                        </label>
+                                        <ErrorMessage name="firstname" component="div" className="text-danger" />
+                                    </div>
+                                    <div className="col">
+                                        <label>
+                                            <Field type="text" name="lastname" placeholder="last Name" />
+                                        </label>
+                                        <ErrorMessage name="lastname" component="div" className="text-danger" />
+                                    </div>
+                                </div>
+                                
+                                <div className="row">
+                                    <div className="col mt-2">
+                                        <button type="submit" className="btn btn-primary m-2">Add User</button>
+                                        <button className="btn btn-secondary" onClick={handleClose}>Close</button>
+                                    </div>
+                                </div>
+                            </Form>
+                        )}
+                            
                         
-                        <div className="row">
-                            <div className="col mt-2">
-                                <button type="submit" className="btn btn-primary m-2">Add User</button>
-                                <button className="btn btn-secondary" onClick={handleClose}>Close</button>
-                            </div>
-                        </div>
-                    </form>
+                    </Formik>
+                    
                 </Modal.Body>
             </Modal>
         </>
--- src/component/EditUser.tsx
+++ src/component/EditUser.tsx
@@ -2,39 +2,49 @@ import { Dispatch, FC, FormEvent, SetStateAction, useState } from "react";
 import { MdModeEdit } from "react-icons/md";
 import Modal from 'react-bootstrap/Modal';
 import { Iperson } from "../App";
+import { Formik, Field, Form, FormikHelpers,ErrorMessage } from 'formik';
+import * as Yup from "yup";
 
 interface Iprops {
     person: Iperson;
     persons: Iperson[];
     setPersons: Dispatch<SetStateAction<Iperson[]>>;
 }
-
+interface MyFormValues {
+    firstname: string;
+    lastname: string;
+}
 const EditUser: FC<Iprops> = ({ person, persons, setPersons }) => {
     const [isShowModal, setIsShowModal] = useState<boolean>(false);
     const handleClose = () => {
-        setfirstName(person.firstName);
-        setLastname(person.lastName);
         setIsShowModal(false);
     }
     const handleShow = () => setIsShowModal(true);
     
-    const [firstName, setfirstName] = useState<string>(person.firstName);
-    const [lastName, setLastname]  = useState<string>(person.lastName);
-
-    const handledSubmitForm = (event: FormEvent<HTMLFormElement>): void => {
-        event.preventDefault();
+    const handledSubmitForm = async (
+        values: MyFormValues,
+        { setSubmitting, resetForm }: FormikHelpers<MyFormValues>
+        )  => {
         const copyPersons = [...persons];
         const index = copyPersons.findIndex(p => p.id === person.id);
         copyPersons[index] = {
             id: person.id,
-            firstName,
-            lastName,
+            firstName:values.firstname,
+            lastName:values.lastname,
             date:new Date()
         };
         
         setPersons(copyPersons);
-        setIsShowModal(false)
+        setIsShowModal(false);
+        
+        setSubmitting(false);
+        resetForm();
     }
+    const validationSchema = Yup.object({
+        firstname: Yup.string().required(" first name is required "),
+        lastname: Yup.string().required(" last name is required ")
+    });
+    const initialValues: MyFormValues = { firstname: person.firstName, lastname: person.lastName };
     return (
         <>
             <MdModeEdit className="m-2" onClick={handleShow} style={{ cursor: "pointer" }} />
@@ -45,39 +55,37 @@ const EditUser: FC<Iprops> = ({ person, persons, setPersons }) => {
                 keyboard={false}
             >
                 <Modal.Body>
-                    <form autoComplete="off" className="container border rounded p-4" onSubmit={(e) => { handledSubmitForm(e) }}>
-                        <div className="row justify-content-center mb-3">Edit user form</div>
-                        <div className="row">
-                            <div className="col">
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="Enter firstName"
-                                    name="firstname"
-                                    value={firstName}
-                                    onChange={(e) => setfirstName(e.target.value)}
-                                    required={true}
-                                />
-                            </div>
-                            <div className="col">
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="Enter lastname"
-                                    name="lastname"
-                                    onChange={(e) => setLastname(e.target.value)}
-                                    value={lastName}
-                                    required={true}
-                                />
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col">
-                                <button type="submit" className="btn btn-primary m-2">Save</button>
-                                <button className="btn btn-secondary" onClick={handleClose}>Close</button>
-                            </div>
-                        </div>
-                    </form>
+                    <Formik
+                        initialValues={initialValues}
+                        validationSchema={validationSchema}
+                        onSubmit={handledSubmitForm}
+                    >
+                        {({ setFieldValue }) => (
+                            <Form autoComplete="off" className="container border rounded p-4" >
+                                <div className="row justify-content-center mb-3">Edit user form</div>
+                                <div className="row">
+                                    <div className="col">
+                                        <label>
+                                            <Field type="text" name="firstname"  placeholder="First Name"/>
+                                        </label>
+                                        <ErrorMessage name="firstname" component="div" className="text-danger" />
+                                    </div>
+                                    <div className="col">
+                                        <label>
+                                            <Field type="text" name="lastname" placeholder="last Name" />
+                                        </label>
+                                        <ErrorMessage name="lastname" component="div" className="text-danger" />
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col">
+                                        <button type="submit" className="btn btn-primary m-2">Save</button>
+                                        <button className="btn btn-secondary" onClick={handleClose}>Close</button>
+                                    </div>
+                                </div>
+                            </Form>
+                        )}
+                    </Formik>
                 </Modal.Body>
             </Modal>
         </>
